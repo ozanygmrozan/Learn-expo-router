9cefc06b610a800e5f45ba8c0af382a92904b963
ozanygmrozan <yagmurozan282@gmail.com>
2023-12-03 20:44:10 +0300

    last

---
+++ app/(tabs)/posts/index.tsx
@@ -0,0 +1,19 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
+
+export default function Posts() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Link style={{ fontSize: 20 }} href={"/post/1"}>
+        open post 1
+      </Link>
+      <Link style={{ fontSize: 20 }} href={"/post/2"}>
+        open post 2
+      </Link>
+      <Link style={{ fontSize: 20 }} href={"/post/3"}>
+        open post 3
+      </Link>
+    </View>
+  );
+}
